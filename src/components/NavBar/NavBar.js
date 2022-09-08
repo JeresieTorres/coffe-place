494e15c4b1f104fc6bde268c263b47f975562f12
Jeresie <jeresiet@gmail.com>
2022-09-07 23:59:14 -0300

    modificacion del navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <header>
+      <span className="nombre">Coffe Place</span>
+      <nav>
+          <a href='#'>INCIO</a>
+
+          <a href='#'>SERVICIOS</a>
+        
+          <a href='#'>CONTACTO</a>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
